a03cb8df6943af598b61326523e22e95e5335b78
Samuel Gonshaw <sam@gonshaw.com>
2015-11-24 21:11:26 +0000

    Added contour finding.

--- src/image_any_to_line.cpp
+++ src/image_any_to_line.cpp
@@ -5,17 +5,66 @@
 #include "image.h"
 using namespace std;
 
+cv::Mat image_in_to_image_cannied(cv::Mat src)
+{
+
+    cv::Mat src_gray, detected_edges;
+    int lowThreshold      = 50; //between 0 and 100 .. TODO: tweak
+    int const ratio       = 3;
+    int const kernel_size = 3;
+
+    /// Convert the image to grayscale
+    cv::cvtColor( src, src_gray, cv::COLOR_BGR2GRAY );
+
+    /// Reduce noise with a kernel 3x3
+    cv::blur( src_gray, detected_edges, cv::Size(3,3) );
+
+    /// Canny detector
+    cv::Canny( detected_edges, detected_edges, lowThreshold, lowThreshold*ratio, kernel_size );
+
+
+  return detected_edges;
+}
+
+
+vector<vector<cv::Point>> image_cannied_to_image_contours(cv::Mat cannied)
+{
+    vector<vector<cv::Point>> contours;
+    cv::findContours(cannied, contours, cv::RETR_LIST, cv::CHAIN_APPROX_TC89_L1, cv::Point(0, 0));
+    return contours;
+}
+
+void debug_display_cannied(cv::Mat cannied)
+{
+    /// Display image
+    cv::namedWindow( "Cannied Image", cv::WINDOW_AUTOSIZE );
+    cv::imshow( "Cannied Image", cannied);
+    /// Wait until user exit program by pressing a key
+    cv::waitKey(0);
+}
+
+void debug_display_contours(cv::Mat src, vector<vector<cv::Point>> contours)
+{
+    /// Draw contours
+    cv::Mat drawing = cv::Mat::ones(src.size(), CV_8UC3);
+    cv::Scalar color = cv::Scalar(0xFF, 0xFF, 0xFF);
+    for( unsigned int i = 0; i< contours.size(); i++ )
+    {
+        cv::drawContours(drawing, contours, i, color);
+    }
+
+    /// Show in a window
+    cv::namedWindow( "Contours", CV_WINDOW_AUTOSIZE );
+    cv::imshow( "Contours", drawing );
+    /// Wait until user exit program by pressing a key
+    cv::waitKey(0);
+}
+
 bool image_any_to_line(string input_filename)
 {
+    cv::Mat src, cannied;
+    vector<vector<cv::Point>> contours;
     
-    cv::Mat src, src_gray;
-    cv::Mat dst, detected_edges;
-
-    int lowThreshold = 50; //between 0 and 100 .. TODO: tweak
-    int ratio = 3;
-    int kernel_size = 3;
-    const char* window_name = "Edge Map";
- 
     /// Load an image
     src = cv::imread(input_filename);
 
@@ -25,29 +74,14 @@ bool image_any_to_line(string input_filename)
         return false;
     }
 
-    /// Create a matrix of the same type and size as src (for dst)
-    dst.create( src.size(), src.type() );
+    //TODO: compile out debug when done
+    cannied = image_in_to_image_cannied(src);
+    debug_display_cannied(cannied);
 
-    /// Convert the image to grayscale
-    cv::cvtColor( src, src_gray, cv::COLOR_BGR2GRAY );
+    contours = image_cannied_to_image_contours(cannied);
+    debug_display_contours(src, contours);
 
-    /// Create a window
-    cv::namedWindow( window_name, cv::WINDOW_AUTOSIZE );
-    /// Reduce noise with a kernel 3x3
-    cv::blur( src_gray, detected_edges, cv::Size(3,3) );
+    //TODO: reduce number of dots, connect contours
 
-    /// Canny detector
-    cv::Canny( detected_edges, detected_edges, lowThreshold, lowThreshold*ratio, kernel_size );
-
-    /// Using Canny's output as a mask, we display our result
-    dst = cv::Scalar::all(0);
-
-    src.copyTo( dst, detected_edges);
-    cv::imshow( window_name, dst );
-
-    /// Wait until user exit program by pressing a key
-    cv::waitKey(0);
-    //TODO: replace image show with saving image for next processing step
-
-  return true;
+    return true;
 }
